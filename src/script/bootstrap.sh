41b4239f8b3ce584088960a1102e77d1d1e8e0c0
Aleš Bernik <psi@gora.si>
2023-02-07 15:54:54 +0100

    ownership changed

--- src/script/bootstrap.sh
+++ src/script/bootstrap.sh
@@ -27,8 +27,8 @@ yti-postgres
 #yti-fuseki = yti-datamodel-database
 termedComponents="termed-api"
 
-YTI_REPO="https://github.com/goraresult/"
-TERMED_REPO="https://github.com/goraresult/"
+YTI_REPO="https://github.com/MJU-PSI/"
+TERMED_REPO="https://github.com/MJU-PSI/"
 
 #clone given repository from git with given branch or master
 clone_component_from_git () {
